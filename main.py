f98cb9ceab716fccfafd88206cbf64f36c6daed2
Robin Ezgiloglu <robin.ezgilioglu@mobilfon.com>
2023-03-07 15:09:41 +0300

    updated

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 from PIL import Image
 import os
 import shutil
-import time 
+import time
+import pathlib
 
 
 def file_operations(TARGET_PATH,SRC_DIR):
@@ -16,14 +17,22 @@ def file_operations(TARGET_PATH,SRC_DIR):
     print("Dosyalar test klasörüne kopyalandı")
 
 
+def get_file_extension(filename):
+    path =  pathlib.Path(filename).suffix
+    return path.replace('.', '')
+
+
 def get_filepath(path):
     return os.path.join(os.getcwd(), path)
+
 
 def compress(root,path,filename):
 
     filepath = get_filepath(path)
-    print(filepath)
+   
     image = Image.open(filepath)
+
+    #extension = get_file_extension(filename); 
 
     try:
         image.save(root+"/"+filename+"_compressed.jpeg", "JPEG", optimize = True, quality = 30)
@@ -35,15 +44,26 @@ def compress(root,path,filename):
 
     return
 
+def create_file_name():
+    return  "errors_"+str(time.strftime("%Y%m%d-%H%M%S"))
+
+
+def crete_error_files():
+
+    logging.basicConfig(filename="compress-image/errors.log", level=logging.ERROR) # log file için belki silinecek
+    mode = 0o666
+    error_path = os.path.join("compress-image",  create_file_name())
+    os.mkdir(error_path, mode)  
+
 
 def main():
 
-    logging.basicConfig(filename="compress-image/errors.log", level=logging.ERROR)
-
+    crete_error_files()
     file_operations(TARGET_PATH,SRC_DIR)
 
     print("İşlem başlatılıyor...")
-    time.sleep(2)
+
+    time.sleep(1)
 
     st = time.time()
     
@@ -58,12 +78,18 @@ def main():
 
     
     elapsed_time = time.time() - st
+    print("---------------------------------------------------")
+    print(str(count) + ' adet dosya  ', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)), ' sürede işlendi.')
 
-    print(str(count) + ' adet dosya  ', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)), ' sürede işlendi')
+
+
+
+
+
 
 if __name__ == "__main__":
     
-    TARGET_PATH = "compress-image\images_test"
+    TARGET_PATH = "compress-image\images_clone"
     SRC_DIR = "compress-image\original_images"
 
     main()
